3bdeb7e3483c0c904019aca67f9391027fa8229f
Krishna-Arjun <krishnaarjunkl@gmail.com>
2024-08-16 21:48:23 +0530

    secondry button font size

--- src/Components/Buttons/Button.jsx
+++ src/Components/Buttons/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({ onClick, name, disable = false, type = "primary" }) => {
       "border  rounded-lg w-full text-sm p-2 bg-gray-900 text-white hover:opacity-90 disabled:opacity-30";
   } else if ("secondary") {
     classNames =
-      "border rounded-lg w-full text-sm p-2 border-gray-900 hover:bg-gray-900  hover:text-white";
+      "border rounded-lg w-full text-sm font-semibold p-2 border-gray-900 hover:bg-gray-900  hover:text-white";
   }
 
   return (
