d834d4e089cb067796a2f912d9c5701e7f6454f8
Krishna-Arjun <krishnaarjunkl@gmail.com>
2024-08-16 13:11:24 +0530

    Shelf dashboard structure

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex flex-col w-full border px-5 py-2">Header</header>
+  );
+};
+
+export default Header;
---
+++ src/Components/Sidebar/NotesBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotesBar = () => {
+  return <aside className="flex flex-col h-full w-72 border">Notes</aside>;
+};
+
+export default NotesBar;
